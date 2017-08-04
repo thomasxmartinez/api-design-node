537c4ed143cde31253fa39b58cdb58c3a07695e8
tommytnez <tommytnez@yahoo.com>
2017-08-03 20:42:30 -0700

    routes practice

--- server/server.js
+++ server/server.js
@@ -21,7 +21,49 @@ app.use(bodyParser.json());
 var lions = [];
 var id = 0;
 
-// TODO: make the REST routes to perform CRUD on lions
+app.get('/lions', function(req, res) {
+  res.send(lions);
+});
+
+// get the parameters from the route
+app.get('/lions/:id', function(req, res) {
+  var lion = _.find(lions, {id: req.params.id});
+  res.json(lion || {});
+});
+
+app.post('/lions', function(req, res) {
+  var lion = req.body;
+  id++;
+  lion.id = id + '';
+  lions.push(lion);
+  res.json(lion);
+});
+
+app.put('/lions/:id', function(req, res) {
+  var update = req.body;
+  if(update.id) {
+    delete update.id;
+  }
+  var lion = _.findIndex(lions, {id: req.params.id});
+  if(!lions[lion]) {
+    res.send();
+  } else {
+    var updatedLion = _.assign(lions[lion], update);
+    res.json(updatedLion);
+  }
+});
+
+app.delete('/lions/:id', function(req, res) {
+  var lion = _.findIndex(lions, {id: req.params.id});
+  if((!lions[lion]) {
+    res.send();
+  } else {
+    var deletedLion = lions[lion];
+    lions.splice(lion, 1);
+    res.json(deletedLion);
+  }
+});
+});
 
 app.listen(3000);
 console.log('on port 3000');
